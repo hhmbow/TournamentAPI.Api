62ecbf30328b08de696204a411df8e00ea3acc16
habib mbow <hhmbow@hotmail.com>
2024-05-26 19:26:49 +0200

    Add project files.

---
+++ Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TournamentAPI.Api.Data.Data;
+using TournamentAPI.Data;
+
+
+
+// Inkludera andra using-direktiv som behövs för din databascontext eller dataseeding-logik
+
+namespace TournamentAPI.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<TournamentAPIApiContext>();
+
+                // Kalla på din seed-metod här
+                await SeedData.SeedTournamentsAsync(context);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TournamentAPI.Api.Data.Data;
+using TournamentAPI.Api.Extensions;
+using TournamentAPI.Core.TournamentAPI.Core.Repositories;
+using TournamentAPI.Data.TournamentAPI.Data.Repositories;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TournamentAPIApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TournamentAPIApiContext") ?? throw new InvalidOperationException("Connection string 'TournamentAPIApiContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddScoped<ITournamentRepository, TournamentRepository>();
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(opt => opt.ReturnHttpNotAcceptable = true)
+    .AddNewtonsoftJson()
+    .AddXmlDataContractSerializerFormatters();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+// Seed data
+app.SeedDataAsync().Wait();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+
+app.Run();
